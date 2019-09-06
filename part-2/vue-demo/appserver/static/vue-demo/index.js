b09643c3137b556b1a969d019784d0954429c38e
Benjamin <Benjamin@Maci.fritz.box>
2019-09-06 17:35:28 +0200

    Add search manager manually

--- part-2/vue-demo/appserver/static/vue-demo/index.js
+++ part-2/vue-demo/appserver/static/vue-demo/index.js
@@ -2,12 +2,13 @@
 
 require([
   'splunkjs/mvc',
+  'splunkjs/mvc/searchmanager',
   '../app/vue-demo/vue-demo/dist/vue-demo-bundle',
   'splunkjs/mvc/simplexml/ready!',
   'splunkjs/ready!'
-], (mvc, VueDemo) => {
+], (mvc, SearchManager, VueDemo) => {
   
   // Initalize VueDemo and pass mvc
-  VueDemo.init(mvc);
+  VueDemo.init(mvc, SearchManager);
 
 });
--- part-2/vue-demo/appserver/static/vue-demo/src/vue-demo.js
+++ part-2/vue-demo/appserver/static/vue-demo/src/vue-demo.js
@@ -1,9 +1,40 @@
 import Vue from 'vue';
 
-export const init = (mvc) => {
+const SearchManagerConfig = {
+  'id': 'search1',
+  'status_buckets': 0,
+  'earliest_time': '-24h@h',
+  'latest_time': 'now',
+  'sample_ratio': 1,
+  'cancelOnUnload': true, 
+  'search': 'index=_internal sourcetype IN (splunkd, splunk_web_service) | timechart count by sourcetype',
+  'app': 'vue-demo',
+  'auto_cancel': 90,
+  'preview': true,
+  'tokenDependencies': {
+  },
+  'runWhenTimeIsUndefined': false
+}
+
+export const init = (mvc, SearchManager) => {
+  // Create and start search
+  const searchManager = new SearchManager(
+    SearchManagerConfig,
+    { tokens: true, tokenNamespace: 'submitted' }
+  ).startSearch();
+
+  // Subscribe to search
+  const results = searchManager
+    .data('results')
+    .on('data', (model, data) => console.log(data.rows));
+
+  // Start search
+
   new Vue({
     data: {
       message: 'Hello World'
-    }
-  }).$mount("#vue-app");
+    },
+
+    template: '<p>{{ message }}</p>'
+  }).$mount('#vue-app');
 }
